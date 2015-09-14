c4ce4b8bc466359db06e10dda9df4c6ce374c7cf
Rob Phoenix <rob.phoenix@bt.com>
2015-09-14 10:42:46 +0100

    added docstring

--- python/rna-transcription/dna.py
+++ python/rna-transcription/dna.py
@@ -1,6 +1,9 @@
 def to_rna(dna):
+    '''
+    Given a DNA strand, returns its RNA complement (per RNA transcription)
+    '''
     rna = ''
     dna_to_rna = {'G': 'C', 'C': 'G', 'A': 'U', 'T': 'A'}
     for char in dna:
-        rna += str(dna_to_rna.get(char))
+        rna += dna_to_rna.get(char)
     return rna
