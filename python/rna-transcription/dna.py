fca8f19e8f20686fff00822e148a064aa9d85c02
Rob Phoenix <rob.phoenix@bt.com>
2015-09-14 11:10:36 +0100

    updated dna.py to use string.join

--- python/rna-transcription/dna.py
+++ python/rna-transcription/dna.py
@@ -2,8 +2,5 @@ def to_rna(dna):
     '''
     Given a DNA strand, returns its RNA complement (per RNA transcription)
     '''
-    rna = ''
     dna_to_rna = {'G': 'C', 'C': 'G', 'A': 'U', 'T': 'A'}
-    for char in dna:
-        rna += dna_to_rna.get(char)
-    return rna
+    return ''.join(dna_to_rna.get(char) for char in dna)
