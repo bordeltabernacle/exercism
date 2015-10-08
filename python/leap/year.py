b2707eff880508d0c71102f61776f8453a84d3c8
Rob Phoenix <rob.phoenix@bt.com>
2015-10-08 09:13:54 +0100

    iterated over year.py after nitpick from @hendi

--- python/leap/year.py
+++ python/leap/year.py
@@ -4,11 +4,9 @@ def is_leap_year(year):
     """
     if year % 4 != 0:
         return False
+    if year % 400 == 0:
+        return True
+    if year % 100 == 0:
+        return False
     else:
-        if year % 100 == 0:
-            if year % 400 == 0:
-                return True
-            else:
-                return False
-        else:
-            return True
+        return True
