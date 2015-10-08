2336be001bebfd95f4d0f3f2fc326fea7cc6254d
Rob Phoenix <rob.phoenix@bt.com>
2015-10-08 09:16:04 +0100

    moved conditionals around in year.py

--- python/leap/year.py
+++ python/leap/year.py
@@ -2,11 +2,11 @@ def is_leap_year(year):
     """
     Returns True if the given year is a leap year, and False if not
     """
-    if year % 4 != 0:
-        return False
     if year % 400 == 0:
         return True
     if year % 100 == 0:
+        return False
+    if year % 4 != 0:
         return False
     else:
         return True
