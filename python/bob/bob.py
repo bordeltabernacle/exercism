27c26413985ca89ba0abd521059e85fa2d0f2a78
Rob Phoenix <robertphoenix@gmail.com>
2015-10-14 11:09:03 +0100

    first iteration of bob.py

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -1,6 +1,17 @@
-#
-# Skeleton file for the Python "Bob" exercise.
-#
-def hey(what):
-
-    return
+def hey(what, response=None):
+    """
+    Given statements, returns the answers of Bob, a lackadaisical teenager.
+    """
+    response_dict = {"stating": "Whatever.",
+                     "shouting": "Whoa, chill out!",
+                     "question": "Sure.",
+                     "silence": "Fine. Be that way!"}
+    if what.strip() == "":
+        response = response_dict["silence"]
+    elif what.isupper():
+        response = response_dict["shouting"]
+    elif what.strip().endswith("?"):
+        response = response_dict["question"]
+    else:
+        response = response_dict["stating"]
+    return response
