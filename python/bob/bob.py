b3cbbe8cd757f37cd5026d38b8647626a50ba876
Rob Phoenix <robertphoenix@gmail.com>
2015-10-14 13:59:46 +0100

    refactored bob.py

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -1,12 +1,33 @@
-def hey(what, response=None):
+def shouting(phrase, response):
+    """ Returns appropriate response to a shouted phrase. """
+    response = response
+    if phrase.isupper():
+        response = "Whoa, chill out!"
+    return response
+
+
+def silence(phrase, response):
+    """ Returns appropriate response to silence. """
+    response = response
+    if phrase.strip() == "":
+        response = "Fine. Be that way!"
+    return response
+
+
+def question(phrase, response):
+    """ Returns appropriate response to a question. """
+    response = response
+    if phrase.strip() and phrase.strip()[-1] == "?":
+        response = "Sure."
+    return response
+
+
+def hey(phrase, response="Whatever."):
     """
-    Given statements, returns the answers of Bob, a lackadaisical teenager.
+    Given statements, returns the answers of Bob,
+    a lackadaisical teenager.
     """
-    if what.strip() == "":
-        return "Fine. Be that way!"
-    elif what.isupper():
-        return "Whoa, chill out!"
-    elif what.strip().endswith("?"):
-        return "Sure."
-    else:
-        return "Whatever."
+    response = silence(phrase, response)
+    response = question(phrase, response)
+    response = shouting(phrase, response)
+    return response
