6229ae2f28a3c3bb8da496bbab8763e74342aba0
Rob Phoenix <robertphoenix@gmail.com>
2015-10-14 14:06:04 +0100

    moved all of Bob's responses into hey function

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -1,33 +1,25 @@
-def shouting(phrase, response):
-    """ Returns appropriate response to a shouted phrase. """
-    response = response
-    if phrase.isupper():
-        response = "Whoa, chill out!"
-    return response
+def shouting(phrase):
+    """ Determines if the given phrase is shouted """
+    return phrase.isupper()
 
 
-def silence(phrase, response):
-    """ Returns appropriate response to silence. """
-    response = response
-    if phrase.strip() == "":
+def silence(phrase):
+    """ Determines if the given phrase is empty """
+    return phrase.strip() == ""
+
+
+def question(phrase):
+    """ Determines if the given phrase is a question """
+    return phrase.strip()[-1] == "?"
+
+
+def hey(phrase):
+    """ Given statements, returns the answers of Bob, a lackadaisical teenager. """
+    response = "Whatever."
+    if silence(phrase):
         response = "Fine. Be that way!"
-    return response
-
-
-def question(phrase, response):
-    """ Returns appropriate response to a question. """
-    response = response
-    if phrase.strip() and phrase.strip()[-1] == "?":
+    elif shouting(phrase):
+        response = "Whoa, chill out!"
+    elif question(phrase):
         response = "Sure."
-    return response
-
-
-def hey(phrase, response="Whatever."):
-    """
-    Given statements, returns the answers of Bob,
-    a lackadaisical teenager.
-    """
-    response = silence(phrase, response)
-    response = question(phrase, response)
-    response = shouting(phrase, response)
     return response
