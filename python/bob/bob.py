1c06f31028a0b5a5f4b9314fca03984af39083d9
Rob Phoenix <robertphoenix@gmail.com>
2015-10-15 11:21:44 +0100

    functional iteration of bob.py

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -1,25 +1,10 @@
-def shouting(phrase):
-    """ Determines if the given phrase is shouted """
-    return phrase.isupper()
-
-
-def silence(phrase):
-    """ Determines if the given phrase is empty """
-    return phrase.strip() == ""
-
-
-def question(phrase):
-    """ Determines if the given phrase is a question """
-    return phrase.strip()[-1] == "?"
-
-
 def hey(phrase):
     """ Given statements, returns the answers of Bob, a lackadaisical teenager. """
     response = "Whatever."
-    if silence(phrase):
-        response = "Fine. Be that way!"
-    elif shouting(phrase):
-        response = "Whoa, chill out!"
-    elif question(phrase):
-        response = "Sure."
+    response = (lambda x, y: "Fine. Be that way!" if x.strip() == '' else y
+                )(phrase, response)
+    response = (lambda x, y: "Sure." if x.strip().endswith("?") else y
+                )(phrase, response)
+    response = (lambda x, y: "Whoa, chill out!" if x.isupper() else y
+                )(phrase, response)
     return response
