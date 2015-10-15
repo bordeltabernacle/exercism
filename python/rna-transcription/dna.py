b290cc974ef97fae405cbe7135881a75dfc722d9
Rob Phoenix <robertphoenix@gmail.com>
2015-10-15 09:51:19 +0100

    new iteration of dna.py

--- python/rna-transcription/dna.py
+++ python/rna-transcription/dna.py
@@ -1,6 +1,9 @@
+from string import maketrans
+
+
 def to_rna(dna):
     '''
     Given a DNA strand, returns its RNA complement (per RNA transcription)
     '''
-    dna_to_rna = {'G': 'C', 'C': 'G', 'A': 'U', 'T': 'A'}
-    return ''.join(dna_to_rna[char] for char in dna)
+    return dna.translate(maketrans("GCAT", "CGUA"))
+
