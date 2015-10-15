53c23f22267a65ae35d2ea898c5fa7f5b66a310a
Rob Phoenix <robertphoenix@gmail.com>
2015-10-15 11:25:50 +0100

    functional iteration of bob.py w/shorter line lengths

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -1,10 +1,16 @@
 def hey(phrase):
-    """ Given statements, returns the answers of Bob, a lackadaisical teenager. """
+    """
+    Given statements, returns the answers of Bob,
+    a lackadaisical teenager.
+    """
     response = "Whatever."
-    response = (lambda x, y: "Fine. Be that way!" if x.strip() == '' else y
+    response = (lambda x, y: "Fine. Be that way!"
+                if x.strip() == '' else y
                 )(phrase, response)
-    response = (lambda x, y: "Sure." if x.strip().endswith("?") else y
+    response = (lambda x, y: "Sure."
+                if x.strip().endswith("?") else y
                 )(phrase, response)
-    response = (lambda x, y: "Whoa, chill out!" if x.isupper() else y
+    response = (lambda x, y: "Whoa, chill out!"
+                if x.isupper() else y
                 )(phrase, response)
     return response
