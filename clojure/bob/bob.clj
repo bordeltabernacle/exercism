cdfe4868ceb3d0e552d7fbca77423a71040d6ff2
Rob Phoenix <robertphoenix@gmail.com>
2015-10-16 11:46:06 +0100

    iterated over bob.clj

--- clojure/bob/bob.clj
+++ clojure/bob/bob.clj
@@ -1,17 +1,12 @@
 (ns bob
   (:require [clojure.string :as str]))
 
-(defn has-letter? [sentence] (some #(Character/isLetter %) sentence))
-(defn yelling?    [sentence] (and (= (str/upper-case sentence) sentence)
-                                  (has-letter? sentence)))
-(defn question?   [sentence] (and (= \? (last sentence))
-                                  (not (yelling? sentence))))
-(defn silence?    [sentence] (str/blank? sentence))
-
 (defn response-for
-  [conversation]
+  [spoken]
   (cond
-    (silence?   conversation) "Fine. Be that way!"
-    (question?  conversation) "Sure."
-    (yelling?   conversation) "Whoa, chill out!"
-    :else "Whatever."))
+    (and (= (.toUpperCase spoken) spoken)
+         (some #(Character/isLetter %) spoken)) "Whoa, chill out!"
+    (str/blank? spoken)                         "Fine. Be that way!"
+    (= (last spoken) \?)                        "Sure."
+    :else                                       "Whatever."))
+
