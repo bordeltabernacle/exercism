675db813333d926905687c4bca8e8a9458fb55eb
Rob Phoenix <robertphoenix@gmail.com>
2015-11-10 10:24:43 +0000

    refactored hamming.py

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -3,9 +3,12 @@ def distance(strandA, strandB):
     Calculates the Hamming difference between two DNA strands
     """
     distance = 0
+
     if len(strandA) != len(strandB):
         raise ValueError("Strands are of unequal length.")
-    for i in range(len(strandA)):
-        if strandA[i] != strandB[i]:
+
+    for x, y in zip(strandA, strandB):
+        if x != y:
             distance += 1
+
     return distance
