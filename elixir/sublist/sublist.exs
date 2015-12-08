e714ed106a936378473ba0d4d0e4cdf0e2b6c433
Rob Phoenix <robertphoenix@gmail.com>
2015-12-08 10:32:12 +0000

    tests passing, except for huge list

--- elixir/sublist/sublist.exs
+++ elixir/sublist/sublist.exs
@@ -1,33 +1,33 @@
 defmodule Sublist do
+
   @doc """
-  Returns whether the first list is a sublist or a superlist of the second list
-  and if not whether it is equal or unequal to the second list.
+  Returns whether the first list is a sublist
+  or a superlist of the second list and if not
+  whether it is equal or unequal to the second list.
   """
-  @spec compare(list, list) :: atom
+  @spec compare(list(), list()) :: :atom
   def compare(a, b) do
     {la, lb} = {length(a), length(b)}
     cond do
       a == b ->
         :equal
-      la < lb && sublist?(a, b, a) ->
+      la < lb && sublist?(a, b, la) ->
         :sublist
-      la > lb && sublist?(b, a, b) ->
+      la > lb && sublist?(b, a, lb) ->
         :superlist
       a != b ->
         :unequal
     end
   end
 
-  def sublist?([], _, _), do: :true
-  def sublist?(_, [], _), do: :false
-  def sublist?(a = [ha|ta], b = [hb|tb], init) do
+  defp sublist?([], _, _), do: :true
+  defp sublist?(_, [], _), do: :false
+  defp sublist?(smaller, larger = [_|tb], len_smaller) do
     cond do
-      a == b ->
+      smaller === Enum.take(larger, len_smaller) ->
         :true
-      ha === hb ->
-        sublist?(ta, tb, init)
-      ha !== hb ->
-        sublist?(init, tb, init)
+      true ->
+        sublist?(smaller, tb, len_smaller)
     end
   end
 
