ebec41ab72f9cbaed4c2cf881bbf8620210c0893
Rob Phoenix <robertphoenix@gmail.com>
2015-12-08 09:43:11 +0000

    first iteration of sublist, failing one test

--- elixir/sublist/sublist.exs
+++ elixir/sublist/sublist.exs
@@ -3,7 +3,35 @@ defmodule Sublist do
   Returns whether the first list is a sublist or a superlist of the second list
   and if not whether it is equal or unequal to the second list.
   """
+  @spec compare(list, list) :: atom
   def compare(a, b) do
-
+    compare(a, b, length(a), length(b))
   end
+
+  defp compare(a, b, la, lb) do
+    cond do
+      a == b && la == lb ->
+        :equal
+      la < lb && sublist?(a, b, la, lb) ->
+        :sublist
+      la > lb && sublist?(b, a, la, lb) ->
+        :superlist
+      a != b ->
+        :unequal
+    end
+  end
+
+  def sublist?([], _, _, _), do: :true
+  def sublist?(_, [], _, _), do: :false
+  def sublist?(a = [ha|ta], b = [hb|tb], la, lb) do
+    cond do
+      a == b ->
+        :true
+      ha === hb ->
+        sublist?(ta, tb, la, lb)
+      ha !== hb ->
+        sublist?(a, tb, la, lb)
+    end
+  end
+
 end
