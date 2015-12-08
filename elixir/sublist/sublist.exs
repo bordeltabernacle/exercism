e5912b56f257c0a81ced5a4dda763f4519905226
Rob Phoenix <robertphoenix@gmail.com>
2015-12-08 10:15:45 +0000

    failing 2 tests

--- elixir/sublist/sublist.exs
+++ elixir/sublist/sublist.exs
@@ -5,32 +5,29 @@ defmodule Sublist do
   """
   @spec compare(list, list) :: atom
   def compare(a, b) do
-    compare(a, b, length(a), length(b))
-  end
-
-  defp compare(a, b, la, lb) do
+    {la, lb} = {length(a), length(b)}
     cond do
-      a == b && la == lb ->
+      a == b ->
         :equal
-      la < lb && sublist?(a, b, la, lb) ->
+      la < lb && sublist?(a, b, a) ->
         :sublist
-      la > lb && sublist?(b, a, la, lb) ->
+      la > lb && sublist?(b, a, b) ->
         :superlist
       a != b ->
         :unequal
     end
   end
 
-  def sublist?([], _, _, _), do: :true
-  def sublist?(_, [], _, _), do: :false
-  def sublist?(a = [ha|ta], b = [hb|tb], la, lb) do
+  def sublist?([], _, _), do: :true
+  def sublist?(_, [], _), do: :false
+  def sublist?(a = [ha|ta], b = [hb|tb], init) do
     cond do
       a == b ->
         :true
       ha === hb ->
-        sublist?(ta, tb, la, lb)
+        sublist?(ta, tb, init)
       ha !== hb ->
-        sublist?(a, tb, la, lb)
+        sublist?(init, tb, init)
     end
   end
 
