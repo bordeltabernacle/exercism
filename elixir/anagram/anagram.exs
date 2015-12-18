6db9b2714f43bce713330fb3492dba6ed5454ac4
Rob Phoenix <robertphoenix@gmail.com>
2015-12-18 12:57:59 +0000

    Sets are not the right method, nearly, but no.

--- elixir/anagram/anagram.exs
+++ elixir/anagram/anagram.exs
@@ -4,6 +4,17 @@ defmodule Anagram do
   """
   @spec match(String.t, [String.t]) :: [String.t]
   def match(base, candidates) do
-  
+    candidates
+    |> Enum.filter(fn(candidate) ->
+      String.length(candidate) == String.length(base) end)
+    |> Enum.filter(fn(candidate) ->
+      Set.subset?(make_set(base), make_set(candidate)) end)
+  end
+
+  defp make_set(str) do
+    str
+    |> String.downcase
+    |> String.split("")
+    |> Enum.into(MapSet.new)
   end
 end
