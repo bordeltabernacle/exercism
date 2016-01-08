a4e2bc25f19899b4de2e98a4dfb4aa037919d554
Rob Phoenix <robertphoenix@gmail.com>
2016-01-08 10:31:46 +0000

    refactored rna_transcription

--- elixir/rna-transcription/dna.exs
+++ elixir/rna-transcription/dna.exs
@@ -1,6 +1,6 @@
 defmodule DNA do
 
-  @dna_to_rna %{71 => 'C', 67 => 'G', 84 => 'A', 65 => 'U'}
+  @dna_to_rna %{?G => ?C, ?C => ?G, ?T => ?A, ?A => ?U}
 
   @doc """
   Transcribes a character list representing DNA nucleotides to RNA
@@ -11,13 +11,5 @@ defmodule DNA do
   'UGAC'
   """
   @spec to_rna([char]) :: [char]
-  def to_rna(dna) do
-    dna
-    |> Enum.reduce('', &(&2 ++ converter(&1)))
-  end
-
-  defp converter(dna) do
-    %{^dna => result} = @dna_to_rna
-    result
-  end
+  def to_rna(dna), do: dna |> Enum.map(&(@dna_to_rna[&1]))
 end
