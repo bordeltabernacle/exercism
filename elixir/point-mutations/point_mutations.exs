472d5b1314fb23d479407efd269651f72fe83fba
Rob Phoenix <robertphoenix@gmail.com>
2016-01-12 15:57:45 +0000

    refactored Elixir point mutations

--- elixir/point-mutations/point_mutations.exs
+++ elixir/point-mutations/point_mutations.exs
@@ -8,7 +8,6 @@ defmodule DNA do
   4
   """
   @spec hamming_distance([char], [char]) :: non_neg_integer
-  # def hamming_distance(strand, strand), do: 0
   def hamming_distance(strand1, strand2) when length(strand1) == length(strand2) do
     Enum.zip(strand1, strand2)
     |> Enum.count(fn({a, b}) -> a !== b end)
