21af760b9dfb4da03fa24ad0ffa964feaa191f74
Rob Phoenix <robertphoenix@gmail.com>
2016-01-14 11:41:13 +0000

    personal refactor of phone_number in elixir

--- elixir/phone-number/phone_number.exs
+++ elixir/phone-number/phone_number.exs
@@ -1,6 +1,6 @@
 defmodule Phone do
 
-  @valid_number_regex ~r"^\D*?1?\D*?(\d{3})\D*(\d{3})\D*(\d{4})$"
+  @bad_number "0000000000"
 
   @doc """
   Remove formatting from a phone number.
@@ -10,35 +10,61 @@ defmodule Phone do
   @spec number(String.t) :: String.t
   def number(raw) do
     raw
-    |> to_number_list
-    |> List.to_string
+    |> scrub_number
+    |> validate
   end
 
-  defp to_number_list(raw) do
-    @valid_number_regex
-    |> Regex.run(raw, [capture: :all_but_first]) || ["000", "000", "0000"]
+  defp scrub_number(raw), do: Regex.replace(~r"\W", raw, "")
+
+  defp validate(number) do
+    number_length = number |> to_char_list |> length
+    cond do
+      number_length == 10 ->
+        number
+      number_length < 10 ->
+        @bad_number
+      number_length == 11 && String.starts_with?(number, "1") ->
+        String.slice(number, 1..11)
+      number_length > 10 ->
+        @bad_number
+    end
   end
 
   @doc """
   Extract the area code from a phone number
-  Returns the first three digits from a phone number,
-  ignoring long distance indicator
   """
   @spec area_code(String.t) :: String.t
   def area_code(raw) do
     raw
-    |> to_number_list
-    |> List.first
+    |> number
+    |> String.slice(0..2)
+  end
+
+  @doc """
+  Extract the prefix code from a phone number
+  """
+  @spec prefix(String.t) :: String.t
+  def prefix(raw) do
+    raw
+    |> number
+    |> String.slice(3..5)
+  end
+
+  @doc """
+  Extract the line number from a phone number
+  """
+  @spec line(String.t) :: String.t
+  def line(raw) do
+    raw
+    |> number
+    |> String.slice(6..9)
   end
 
   @doc """
   Pretty print a phone number
-  Wraps the area code in parentheses and separates
-  exchange and subscriber number with a dash.
   """
   @spec pretty(String.t) :: String.t
   def pretty(raw) do
-    [area_code, prefix, main] = to_number_list(raw)
-    "(#{area_code}) #{prefix}-#{main}"
+    "(#{area_code(raw)}) #{prefix(raw)}-#{line(raw)}"
   end
 end
