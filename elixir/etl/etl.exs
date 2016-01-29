fa653c8da53b1122bd1fc88d262dbe350e47de1a
Rob Phoenix <robertphoenix@gmail.com>
2016-01-29 10:00:03 +0000

    complete etl exercise

--- elixir/etl/etl.exs
+++ elixir/etl/etl.exs
@@ -1,14 +1,7 @@
 defmodule ETL do
-  @doc """
-  Transform an index into an inverted index.
-
-  ## Examples
-
-  iex> ETL.transform(%{"a" => ["ABILITY", "AARDVARK"], "b" => ["BALLAST", "BEAUTY"]})
-  %{"ability" => "a", "aardvark" => "a", "ballast" => "b", "beauty" =>"b"}
-  """
-  @spec transform(Dict.t) :: map()
   def transform(input) do
-
+    for {k, v} <- input, elem <- v,
+    into: %{},
+    do: {String.downcase(elem), k}
   end
 end
--- elixir/etl/etl_test.exs
+++ elixir/etl/etl_test.exs
@@ -18,7 +18,7 @@ defmodule TransformTest do
     assert ETL.transform(old) == expected
   end
 
-  @tag :pending
+  # @tag :pending
   test "transform more values" do
     old = %{1 => ["WORLD", "GSCHOOLERS"]}
     expected = %{"world" => 1, "gschoolers" => 1}
@@ -26,7 +26,7 @@ defmodule TransformTest do
     assert ETL.transform(old) == expected
   end
 
-  @tag :pending
+  # @tag :pending
   test "more keys" do
     old = %{1 => ["APPLE", "ARTICHOKE"], 2 => ["BOAT", "BALLERINA"]}
     expected = %{
@@ -39,7 +39,7 @@ defmodule TransformTest do
     assert ETL.transform(old) == expected
   end
 
-  @tag :pending
+  # @tag :pending
   test "full dataset" do
     old = %{
       1 =>  ~W(A E I O U L N R S T),
