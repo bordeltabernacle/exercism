402a418301802a50df91a86b06af79ce80e7f2bf
Rob Phoenix <robertphoenix@gmail.com>
2016-06-21 16:40:37 +0100

    complete first go exercise, start going over old python exercises

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -3,15 +3,10 @@ def hey(phrase):
     Given statements, returns the answers of Bob,
     a lackadaisical teenager.
     """
-    response = "Whatever."
-    response = (lambda x, y: "Fine. Be that way!"
-                if x.strip() == ''
-                else y)(phrase, response)
-    response = (lambda x, y: "Sure."
-                if x.strip().endswith("?")
-                else y)(phrase, response)
-    response = (lambda x, y: "Whoa, chill out!"
-                if x.isupper()
-                else y)(phrase, response)
-
-    return response
+    if phrase.strip() == '':
+        return "Fine. Be that way!"
+    if phrase.isupper():
+        return "Whoa, chill out!"
+    if phrase.strip().endswith("?"):
+        return "Sure."
+    return "Whatever."
