b5e61164ce9e0ef56629c1a74fbf459eec4d6a3e
Rob Phoenix <robertphoenix@gmail.com>
2016-06-24 09:32:48 +0100

    one liner

--- python/pangram/pangram.py
+++ python/pangram/pangram.py
@@ -2,8 +2,4 @@ import string
 
 
 def is_pangram(sentence):
-    alphabet = list(string.ascii_lowercase)
-    for letter in alphabet:
-        if letter not in sentence.lower():
-            return False
-    return True
+    return all(letter in sentence.lower() for letter in list(string.ascii_lowercase))
