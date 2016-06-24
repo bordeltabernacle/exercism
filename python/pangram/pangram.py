2d8b94be640caa262373a248d6e7d2eeaa65a56e
Rob Phoenix <robertphoenix@gmail.com>
2016-06-24 09:24:15 +0100

    refactored to return False as soon as letter found to be missing

--- python/pangram/pangram.py
+++ python/pangram/pangram.py
@@ -3,6 +3,7 @@ import string
 
 def is_pangram(sentence):
     alphabet = list(string.ascii_lowercase)
-    test = sum(1 for letter in alphabet if letter in sentence.lower())
-    result = True if test == 26 else False
-    return result
+    for letter in alphabet:
+        if letter not in sentence.lower():
+            return False
+    return True
