f900503a9d711a4e131b8c1c95fcbbfeb0d77db4
Rob Phoenix <robertphoenix@gmail.com>
2016-06-29 16:07:35 +0100

    initial attempt at run-length-encoding

---
+++ python/run-length-encoding/run_length.py
@@ -0,0 +1,34 @@
+import string
+
+
+def encode(data):
+    prev, encoded, count = '', '', 0
+    for el in data:
+        if el != prev:
+            if count > 1:
+                encoded += str(count)
+            encoded += prev
+            prev = el
+            count = 0
+        if el == prev:
+            count += 1
+    if count > 1:
+        encoded += str(count)
+    encoded += prev
+    return encoded
+
+
+def decode(data):
+    decoded = ''
+    num = string.digits
+    count = ''
+    for el in data:
+        if el in num:
+            count += el
+        else:
+            if count == '':
+                decoded += el
+            else:
+                decoded += (el * int(count))
+                count = ''
+    return decoded
