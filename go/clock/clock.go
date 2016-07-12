0001d92eccadcb762daaae5e3d05103b92d5f9b1
Rob Phoenix <robertphoenix@gmail.com>
2016-07-12 10:00:10 +0100

    started clock again from scratch
    I felt bad that I threw in the towel so quickly, read some more
    and had another go at the clock exercise from scratch, trying as
    hard as I could to forget some of what I knew

--- go/clock/clock.go
+++ go/clock/clock.go
@@ -6,31 +6,27 @@ import (
 
 const testVersion = 4
 
-// define the Clock type as a total number of minutes
-type Clock int
+type Clock struct {
+	h, m int
+}
 
 func New(h, m int) Clock {
-	// calculate total number of minutes from given hours & minutes
-	// ensure this total is within a single 24 hour (1440 minute)
-	// period by taking only the number of minutes left over after
-	// removing any multiples of 1440
-	c := Clock((h*60 + m) % 1440)
-	// if the calculated time is negative add a days worth of minutes
-	// so that it is moving forward from 0 rather than backwards
-	if c < 0 {
-		c += 1440
+	h = (h + (m / 60)) % 24
+	m = m % 60
+	if m < 0 {
+		m += 60
+		h--
 	}
-	return c
+	if h < 0 {
+		h += 24
+	}
+	return Clock{h, m}
 }
 
 func (c Clock) Add(m int) Clock {
-	// convert the clock type to an int and add the given minutes to it
-	return New(0, int(c)+m)
+	return New(c.h, (c.m + m))
 }
 
 func (c Clock) String() string {
-	// pull out the hours by dividing by 60 mins
-	// pull out the minutes by calculating what's left
-	// pretty print the results
-	return fmt.Sprintf("%02d:%02d", c/60, c%60)
+	return fmt.Sprintf("%02d:%02d", c.h, c.m)
 }
