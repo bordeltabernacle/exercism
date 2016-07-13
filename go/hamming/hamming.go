f69907aa518e41d1f8a2ff7e9825400dcf4425bc
Rob Phoenix <robertphoenix@gmail.com>
2016-07-13 11:40:51 +0100

    remove unnecessary conversion of strings to slices

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -1,23 +1,18 @@
 package hamming
 
-import (
-	"errors"
-	"strings"
-)
+import "errors"
 
 const testVersion = 4
 
 func Distance(a, b string) (int, error) {
-	aSplit := strings.Split(a, "")
-	bSplit := strings.Split(b, "")
-	distance := 0
-	if len(aSplit) != len(bSplit) {
-		return 0, errors.New("math: square root of negative number")
+	var dist int
+	if len(a) != len(b) {
+		return 0, errors.New("DNA strands of unequal length")
 	}
-	for c := 0; c < len(aSplit); c++ {
-		if aSplit[c] != bSplit[c] {
-			distance++
+	for i := 0; i < len(a); i++ {
+		if a[i] != b[i] {
+			dist++
 		}
 	}
-	return distance, nil
+	return dist, nil
 }
