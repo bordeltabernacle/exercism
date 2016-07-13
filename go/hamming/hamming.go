069d3abcdf98bd566d62ed44400fccda48b1307d
Rob Phoenix <robertphoenix@gmail.com>
2016-07-13 11:33:30 +0100

    first iteration of hamming.go

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -1,7 +1,23 @@
 package hamming
 
+import (
+	"errors"
+	"strings"
+)
+
 const testVersion = 4
 
 func Distance(a, b string) (int, error) {
-	// your code here!
+	aSplit := strings.Split(a, "")
+	bSplit := strings.Split(b, "")
+	distance := 0
+	if len(aSplit) != len(bSplit) {
+		return 0, errors.New("math: square root of negative number")
+	}
+	for c := 0; c < len(aSplit); c++ {
+		if aSplit[c] != bSplit[c] {
+			distance++
+		}
+	}
+	return distance, nil
 }
