412a73102ce9798f3df7bfe0f06654cd9d2ba7b5
Rob Phoenix <robertphoenix@gmail.com>
2016-07-13 11:57:19 +0100

    add docstrings

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -1,3 +1,4 @@
+// Calculates the Hamming difference between two DNA strands.
 package hamming
 
 import "errors"
@@ -5,16 +6,16 @@ import "errors"
 const testVersion = 4
 
 func Distance(a, b string) (dist int, err error) {
-
+	// check DNA strands are the same length
 	if len(a) != len(b) {
 		return dist, errors.New("DNA strands of unequal length")
 	}
-
+	// compare each nucleotide in strands
+	// incrementing dist if not equal
 	for i := range a {
 		if a[i] != b[i] {
 			dist++
 		}
 	}
-
 	return
 }
