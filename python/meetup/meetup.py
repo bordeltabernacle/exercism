64de1dcd3ef7a677292a244568888f98a8d5f20c
Rob Phoenix <robertphoenix@gmail.com>
2016-07-19 12:56:40 +0100

    fix dotw bug

--- python/meetup/meetup.py
+++ python/meetup/meetup.py
@@ -17,14 +17,14 @@ def meetup_day(year, month, dotw, when):
     (_, no_of_days) = calendar.monthrange(year, month)
     if when == "last":
         while no_of_days > 1:
-            if date(year, month, no_of_days).weekday() == DOTW.index(dotw.lower()):
+            if date(year, month, no_of_days).weekday() == DOTW.index(dotw):
                 return date(year, month, no_of_days)
             no_of_days -= 1
         raise Exception
     else:
         i = WEEK[when]
         while i < no_of_days:
-            if date(year, month, i).weekday() == DOTW.index(dotw.lower()):
+            if date(year, month, i).weekday() == DOTW.index(dotw):
                 return date(year, month, i)
             i += 1
         raise Exception
