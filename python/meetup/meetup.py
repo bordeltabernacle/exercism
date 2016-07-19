bf6b522e19beafe09b333cb5be99e578f438339f
Rob Phoenix <robertphoenix@gmail.com>
2016-07-19 12:42:24 +0100

    change DOTW from dict to list

--- python/meetup/meetup.py
+++ python/meetup/meetup.py
@@ -2,16 +2,13 @@ from datetime import date
 from calendar import monthrange
 
 
-DOTW = {
-        "monday": 0,
-        "tuesday": 1,
-        "wednesday": 2,
-        "thursday": 3,
-        "friday": 4,
-        "saturday": 5,
-        "sunday": 6
-}
-
+DOTW = ["monday",
+        "tuesday",
+        "wednesday",
+        "thursday",
+        "friday",
+        "saturday",
+        "sunday"]
 WEEK = {
         "1st": 1,
         "2nd": 8,
@@ -25,13 +22,13 @@ def meetup_day(year, month, dotw, when):
     (_, no_of_days) = monthrange(year, month)
     if when == "last":
         while no_of_days > 1:
-            if date(year, month, no_of_days).weekday() == DOTW[dotw.lower()]:
+            if date(year, month, no_of_days).weekday() == DOTW.index(dotw.lower()):
                 return date(year, month, no_of_days)
             no_of_days -= 1
     else:
         i = WEEK[when]
         while i < no_of_days:
-            if date(year, month, i).weekday() == DOTW[dotw.lower()]:
+            if date(year, month, i).weekday() == DOTW.index(dotw.lower()):
                 return date(year, month, i)
             i += 1
 
