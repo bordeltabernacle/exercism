67402f0ef2200cfc091cefcbf828aaa300268b91
Rob Phoenix <robertphoenix@gmail.com>
2016-07-19 13:45:06 +0100

    simplify

--- python/meetup/meetup.py
+++ python/meetup/meetup.py
@@ -16,13 +16,12 @@ def meetup_day(year, month, dotw, when):
     DOTW = [d for d in calendar.day_name]
     (_, days) = calendar.monthrange(year, month)
     if when == "last":
-        return _get_day(year, month, DOTW.index(dotw), reversed(range(days - 6, days + 1)))
-    else:
-        return _get_day(year, month, DOTW.index(dotw), range(WEEK[when], days + 1))
+        return _get_day(year, month, DOTW.index(dotw),
+                reversed(range(days - 6, days + 1)))
+    return _get_day(year, month, DOTW.index(dotw),
+            range(WEEK[when], days + 1))
 
 
 def _get_day(year, month, dotw, days):
-    for day in days:
-        if date(year, month, day).weekday() == dotw:
-            return date(year, month, day)
-    raise Exception
+    day = [day for day in days if date(year, month, day).weekday() == dotw][0]
+    return date(year, month, day)
