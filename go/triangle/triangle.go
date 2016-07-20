69f80f56f09f64cf65f813177d98ae689fab1cee
Rob Phoenix <robertphoenix@gmail.com>
2016-07-20 14:34:35 +0100

    move logic out into separate functions

--- go/triangle/triangle.go
+++ go/triangle/triangle.go
@@ -27,15 +27,34 @@ func KindFromSides(a, b, c float64) Kind {
 }
 
 func triTest(a, b, c float64) bool {
+	return isNotInf(a, b, c) &&
+		isNotNan(a, b, c) &&
+		greaterThanZero(a, b, c) &&
+		meetsTriIneq(a, b, c)
+}
+
+func isNotInf(a, b, c float64) bool {
 	if math.IsInf(a, 0) || math.IsInf(b, 0) || math.IsInf(c, 0) {
 		return false
 	}
+	return true
+}
+
+func isNotNan(a, b, c float64) bool {
 	if math.IsNaN(a) || math.IsNaN(b) || math.IsNaN(c) {
 		return false
 	}
+	return true
+}
+
+func greaterThanZero(a, b, c float64) bool {
 	if a <= 0 || b <= 0 || c <= 0 {
 		return false
 	}
+	return true
+}
+
+func meetsTriIneq(a, b, c float64) bool {
 	if a+b < c || b+c < a || c+a < b {
 		return false
 	}
