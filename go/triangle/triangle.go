066a1773618a1beb8be93b8115f74ffa1946fbc4
Rob Phoenix <robertphoenix@gmail.com>
2016-07-20 14:57:42 +0100

    test each side rather than each test

--- go/triangle/triangle.go
+++ go/triangle/triangle.go
@@ -32,28 +32,11 @@ func KindFromSides(a, b, c float64) Kind {
 
 // triTest tests if the given sides form a valid triangle
 func triTest(a, b, c float64) bool {
-	return isNotInf(a, b, c) && isNotNaN(a, b, c) && greaterThanZero(a, b, c) && meetsTriIneq(a, b, c)
+	return validSide(a) && validSide(b) && validSide(c) && meetsTriIneq(a, b, c)
 }
 
-// isNotInf checks all sides are not infinity
-func isNotInf(a, b, c float64) bool {
-	if math.IsInf(a, 0) || math.IsInf(b, 0) || math.IsInf(c, 0) {
-		return false
-	}
-	return true
-}
-
-// isNotNaN checks all sides are not NaN
-func isNotNaN(a, b, c float64) bool {
-	if math.IsNaN(a) || math.IsNaN(b) || math.IsNaN(c) {
-		return false
-	}
-	return true
-}
-
-// greaterThanZero checks all sides are greater than zero
-func greaterThanZero(a, b, c float64) bool {
-	if a <= 0 || b <= 0 || c <= 0 {
+func validSide(s float64) bool {
+	if math.IsInf(s, 0) || math.IsNaN(s) || s <= 0 {
 		return false
 	}
 	return true
