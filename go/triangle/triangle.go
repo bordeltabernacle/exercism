d6cb86afb20bb835a5bb9b94f5f11ef63bdeee30
Rob Phoenix <robertphoenix@gmail.com>
2016-07-20 12:02:27 +0100

    first iteration of triangle.go

--- go/triangle/triangle.go
+++ go/triangle/triangle.go
@@ -1,17 +1,49 @@
 package triangle
 
+import "math"
+
 const testVersion = 2
 
-// Code this function.
-func KindFromSides(a, b, c float64) Kind
+type Kind string
 
-// Notice it returns this type.  Pick something suitable.
-type Kind
+const (
+	NaT Kind = "not a triangle"
+	Equ Kind = "equilateral"
+	Iso Kind = "isosceles"
+	Sca Kind = "scalene"
+)
 
-// Pick values for the following identifiers used by the test program.
-NaT // not a triangle
-Equ // equilateral
-Iso // isosceles
-Sca // scalene
+func KindFromSides(a, b, c float64) Kind {
+	if triTest(a, b, c) {
+		if a == b && b == c {
+			return Equ
+		}
+		if a == b || b == c || a == c {
+			return Iso
+		}
+		return Sca
+	}
+	return NaT
+}
 
-// Organize your code for readability.
+func triTest(a, b, c float64) bool {
+	if math.IsInf(a, 0) || math.IsInf(b, 0) || math.IsInf(c, 0) {
+		return false
+	}
+	if math.IsNaN(a) || math.IsNaN(b) || math.IsNaN(c) {
+		return false
+	}
+	if a <= 0 || b <= 0 || c <= 0 {
+		return false
+	}
+	if a+b < c {
+		return false
+	}
+	if b+c < a {
+		return false
+	}
+	if c+a < b {
+		return false
+	}
+	return true
+}
