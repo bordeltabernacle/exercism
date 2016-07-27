310b2f9a2e7a7cff6febde2f20b2ceff036dc038
Rob Phoenix <robertphoenix@gmail.com>
2016-07-27 11:19:54 +0100

    stop tracking hours, from @chrisftw solution

--- python/clock/clock.py
+++ python/clock/clock.py
@@ -1,23 +1,14 @@
 class Clock:
     def __init__(self, hours, minutes):
-        self.hours = hours
-        self.minutes = minutes
-        self.time()
-
-    def time(self):
-        t = (self.hours * 60 + self.minutes) % 1440
-        if t < 0:
-            t += 1440
-        self.hours = int(t / 60)
-        self.minutes = int(t % 60)
-        return self
+        self.minutes = (hours * 60 + minutes) % 1440
 
     def add(self, minutes):
-        self.minutes += minutes
-        return self.time()
+        self.minutes = (self.minutes + minutes) % 1440
+        return self
 
     def __repr__(self):
-        return "{h:0>2d}:{m:0>2d}".format(h=self.hours, m=self.minutes)
+        return "{h:0>2d}:{m:0>2d}".format(
+                h=int(self.minutes/60), m=int(self.minutes%60))
 
     def __eq__(self, other):
         return repr(self) == repr(other)
