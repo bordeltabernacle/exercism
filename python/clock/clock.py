dbfd49938e1e107df2171ac7a538df2621c5bf01
Rob Phoenix <robertphoenix@gmail.com>
2016-07-27 11:27:43 +0100

    minor improvements

--- python/clock/clock.py
+++ python/clock/clock.py
@@ -7,8 +7,7 @@ class Clock:
         return self
 
     def __repr__(self):
-        return "{h:0>2d}:{m:0>2d}".format(
-                h=int(self.minutes/60), m=int(self.minutes%60))
+        return "{:02}:{:02}".format(self.minutes // 60, self.minutes % 60)
 
     def __eq__(self, other):
         return repr(self) == repr(other)
