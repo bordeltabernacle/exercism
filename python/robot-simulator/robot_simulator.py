09ccb44ce25a8bff542d19a1d5b4f5cc2897c66b
Rob Phoenix <robertphoenix@gmail.com>
2016-08-10 15:33:46 +0100

    various improvements to robot

--- python/robot-simulator/robot_simulator.py
+++ python/robot-simulator/robot_simulator.py
@@ -3,25 +3,24 @@ NORTH, EAST, SOUTH, WEST = range(4)
 
 class Robot:
     def __init__(self, bearing=NORTH, x=0, y=0):
-        self._directions = [NORTH, EAST, SOUTH, WEST]
         self.coordinates = (x, y)
         self.bearing = bearing
 
     def turn_right(self):
-        self.bearing = self._directions[(self.bearing + 1) % 4]
+        self.bearing = (self.bearing + 1) % 4
 
     def turn_left(self):
-        self.bearing = self._directions[self.bearing - 1]
+        self.bearing = (self.bearing - 1) % 4
 
     def advance(self):
-        actions = {NORTH: (self.coordinates[0], self.coordinates[1] + 1),
-                   SOUTH: (self.coordinates[0], self.coordinates[1] - 1),
-                   EAST: (self.coordinates[0] + 1, self.coordinates[1]),
-                   WEST: (self.coordinates[0] - 1, self.coordinates[1])}
-        self.coordinates = actions[self.bearing]
+        self.coordinates = {
+            NORTH: (self.coordinates[0], self.coordinates[1] + 1),
+            SOUTH: (self.coordinates[0], self.coordinates[1] - 1),
+            EAST: (self.coordinates[0] + 1, self.coordinates[1]),
+            WEST: (self.coordinates[0] - 1, self.coordinates[1])
+        }[self.bearing]
 
     def simulate(self, movements):
-        actions = {"R": self.turn_right,
-                   "L": self.turn_left,
-                   "A": self.advance}
-        [actions[movement]() for movement in movements]
+        [{"R": self.turn_right,
+          "L": self.turn_left,
+          "A": self.advance}[movement]() for movement in movements]
