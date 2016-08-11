69dc8c85505895f64ecb5d4660b20e382f19d603
Rob Phoenix <robertphoenix@gmail.com>
2016-08-11 11:18:20 +0100

    second iteration of atbash_cipher.py

--- python/atbash-cipher/atbash_cipher.py
+++ python/atbash-cipher/atbash_cipher.py
@@ -1,29 +1,16 @@
 import string
 
 
-ALPHABET = list(string.ascii_lowercase)
-
-
 def encode(plaintext):
-    encoded = ""
-    for c in plaintext.lower():
-        if c == " " or c in string.punctuation:
-            pass
-        elif c in string.digits:
-            encoded += c
-        else:
-            i = ALPHABET.index(c)
-            encoded += ALPHABET[-(i + 1)]
-    encoded = " ".join(encoded[i:i+5] for i in range(0, len(encoded), 5))
-    return encoded
+    encoded = _codec(plaintext.lower())
+    return " ".join(encoded[i:i+5] for i in range(0, len(encoded), 5))
 
 
 def decode(cyphertext):
-    decoded = ""
-    for c in cyphertext.lower():
-        if c == " ":
-            pass
-        else:
-            i = ALPHABET.index(c)
-            decoded += ALPHABET[-(i + 1)]
-    return decoded
+    return _codec(cyphertext)
+
+
+def _codec(text):
+    a = string.ascii_lowercase
+    t = str.maketrans(a, a[::-1])
+    return "".join(list(c.translate(t) if c.isalnum() else "" for c in text))
