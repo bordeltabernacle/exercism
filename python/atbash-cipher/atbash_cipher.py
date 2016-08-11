43dd340ccfd677c8e6e656131772889a24649c13
Rob Phoenix <robertphoenix@gmail.com>
2016-08-11 11:19:47 +0100

    remove unnecessary list()

--- python/atbash-cipher/atbash_cipher.py
+++ python/atbash-cipher/atbash_cipher.py
@@ -13,4 +13,4 @@ def decode(cyphertext):
 def _codec(text):
     a = string.ascii_lowercase
     t = str.maketrans(a, a[::-1])
-    return "".join(list(c.translate(t) if c.isalnum() else "" for c in text))
+    return "".join(c.translate(t) if c.isalnum() else "" for c in text)
