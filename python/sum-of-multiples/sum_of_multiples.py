ef7a669336589de2969da7304487bfb4ee3693e5
Rob Phoenix <robertphoenix@gmail.com>
2016-08-26 11:30:25 +0100

    second iteration

--- python/sum-of-multiples/sum_of_multiples.py
+++ python/sum-of-multiples/sum_of_multiples.py
@@ -1,7 +1,3 @@
 def sum_of_multiples(n, muls):
-    res = []
-    for i in range(n):
-        for m in muls:
-            if m != 0 and i % m == 0:
-                res.append(i)
-    return sum(set(res))
+    muls = muls[1:] if muls[0] == 0 else muls
+    return sum(i for i in range(n) if any(i % m == 0 for m in muls))
