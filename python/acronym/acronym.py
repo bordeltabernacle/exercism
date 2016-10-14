5edc1147f53504d3ce7c8a4e2cde59037c7df44c
Rob Phoenix <robertphoenix@gmail.com>
2016-10-14 22:20:43 +0100

    reiterate acronym

--- python/acronym/acronym.py
+++ python/acronym/acronym.py
@@ -1,7 +1,5 @@
 import re
 
 def abbreviate(phrase):
-    findall_words = re.findall(re.compile(r"\w+\b"), phrase)
-    findall_camel_case = [re.findall(re.compile(r"[A-Z][a-z]+|\w+"), r) for r in findall_words]
-    acronym = "".join((word[0] for words in findall_camel_case for word in words)).upper()
-    return acronym
+    pattern = "[A-Z]+[a-z]*|[a-z]+"
+    return "".join(word[0] for word in re.findall(pattern, phrase)).upper()
