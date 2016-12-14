1a125314c97805af66a0f331735575cebf1084bf
Rob Phoenix <robphoenix@protonmail.com>
2016-12-14 12:51:33 +0000

    be more explicit

--- go/pangram/pangram.go
+++ go/pangram/pangram.go
@@ -6,7 +6,7 @@ const testVersion = 1
 
 func IsPangram(sentence string) bool {
 	sentence = strings.ToLower(sentence)
-	for i := 97; i < 123; i++ {
+	for i := int('a'); i <= int('z'); i++ {
 		if strings.Contains(sentence, string(i)) == false {
 			return false
 		}
