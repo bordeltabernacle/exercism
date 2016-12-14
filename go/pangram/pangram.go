1b8dd83dd408142ce9774c64b499a9e8b6b7418e
Rob Phoenix <robphoenix@protonmail.com>
2016-12-14 12:55:39 +0000

    improve boolean

--- go/pangram/pangram.go
+++ go/pangram/pangram.go
@@ -7,7 +7,7 @@ const testVersion = 1
 func IsPangram(sentence string) bool {
 	sentence = strings.ToLower(sentence)
 	for i := int('a'); i <= int('z'); i++ {
-		if strings.Contains(sentence, string(i)) == false {
+		if !strings.Contains(sentence, string(i)) {
 			return false
 		}
 	}
