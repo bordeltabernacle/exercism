14b92e418c8f98dfa9916b296352c7e235c14821
Rob Phoenix <robphoenix@protonmail.com>
2016-12-14 12:49:37 +0000

    remove alphabet const

--- go/pangram/pangram.go
+++ go/pangram/pangram.go
@@ -2,13 +2,12 @@ package pangram
 
 import "strings"
 
-const alphabet = "abcdefghijklmnopqrstuvwxyz"
 const testVersion = 1
 
 func IsPangram(sentence string) bool {
 	sentence = strings.ToLower(sentence)
-	for _, letter := range alphabet {
-		if strings.Contains(sentence, string(letter)) == false {
+	for i := 97; i < 123; i++ {
+		if strings.Contains(sentence, string(i)) == false {
 			return false
 		}
 	}
