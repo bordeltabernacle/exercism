76d394ee94377ece099dd8c00828de71abcf442c
Rob Phoenix <robphoenix@protonmail.com>
2016-12-14 12:59:59 +0000

    improvements

--- go/pangram/pangram.go
+++ go/pangram/pangram.go
@@ -5,7 +5,13 @@ import "strings"
 const testVersion = 1
 
 func IsPangram(sentence string) bool {
+
+	if len(sentence) < 26 {
+		return false
+	}
+
 	sentence = strings.ToLower(sentence)
+
 	for i := int('a'); i <= int('z'); i++ {
 		if !strings.Contains(sentence, string(i)) {
 			return false
