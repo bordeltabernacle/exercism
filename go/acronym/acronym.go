1b4b854914b83675287282591eeca7302e601476
Rob Phoenix <robphoenix@protonmail.com>
2016-12-14 12:25:15 +0000

    simplify

--- go/acronym/acronym.go
+++ go/acronym/acronym.go
@@ -10,10 +10,9 @@ const testVersion int = 1
 func abbreviate(name string) (abbreviation string) {
 	pattern := "[A-Z]+[a-z]*|[a-z]+"
 	re := regexp.MustCompile(pattern)
-	matches := re.FindAllString(name, -1)
-	for _, match := range matches {
-		firstLetter := strings.Split(match, "")[0]
-		abbreviation += strings.ToUpper(firstLetter)
+	match := re.FindAllString(name, -1)
+	for _, m := range match {
+		abbreviation += strings.ToUpper(m[:1])
 	}
 	return
 }
