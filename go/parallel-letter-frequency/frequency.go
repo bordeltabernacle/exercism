6cdac5e4af96ba502e36153f3bebadbcb47b602e
Rob Phoenix <robphoenix@protonmail.com>
2017-01-31 17:13:48 +0000

    tidy up frequency.go

--- go/parallel-letter-frequency/frequency.go
+++ go/parallel-letter-frequency/frequency.go
@@ -2,29 +2,30 @@ package letter
 
 type FreqMap map[rune]int
 
-func ConcurrentFrequency(texts []string) FreqMap {
-	c := make(chan string)
-	m := FreqMap{}
-	go populateChan(c, texts)
-	for s := range c {
-		for _, r := range s {
-			m[r]++
-		}
-	}
-	return m
-}
-
-func populateChan(c chan string, texts []string) {
-	for _, t := range texts {
-		c <- t
-	}
-	close(c)
-}
-
 func Frequency(s string) FreqMap {
 	m := FreqMap{}
 	for _, r := range s {
 		m[r]++
 	}
+	return m
+}
+
+func ConcurrentFrequency(texts []string) FreqMap {
+	c := make(chan FreqMap)
+	m := FreqMap{}
+
+	go func(c chan FreqMap, texts []string) {
+		for _, t := range texts {
+			c <- Frequency(t)
+		}
+		close(c)
+	}(c, texts)
+
+	for s := range c {
+		for letter, count := range s {
+			m[letter] += count
+		}
+	}
+
 	return m
 }
