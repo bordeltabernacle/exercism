c7d00a61bbde965c152085fb42282d583b2a980b
Rob Phoenix <robphoenix@protonmail.com>
2017-01-31 16:59:18 +0000

    amateur night in concurrency city

--- go/parallel-letter-frequency/frequency.go
+++ go/parallel-letter-frequency/frequency.go
@@ -2,6 +2,25 @@ package letter
 
 type FreqMap map[rune]int
 
+func ConcurrentFrequency(texts []string) FreqMap {
+	c := make(chan string)
+	m := FreqMap{}
+	go populateChan(c, texts)
+	for s := range c {
+		for _, r := range s {
+			m[r]++
+		}
+	}
+	return m
+}
+
+func populateChan(c chan string, texts []string) {
+	for _, t := range texts {
+		c <- t
+	}
+	close(c)
+}
+
 func Frequency(s string) FreqMap {
 	m := FreqMap{}
 	for _, r := range s {
