6098aad345c7aa652dc83702e58e403761122ee1
Rob Phoenix <robphoenix@protonmail.com>
2017-02-03 09:10:16 +0000

    change to slice of runes

--- go/scrabble-score/scrabble_score.go
+++ go/scrabble-score/scrabble_score.go
@@ -1,41 +1,41 @@
 package scrabble
 
-import "strings"
+import "unicode"
 
 const testVersion = 4
 
-var scores = map[string]int{
-	"a": 1,
-	"b": 3,
-	"c": 3,
-	"d": 2,
-	"e": 1,
-	"f": 4,
-	"g": 2,
-	"h": 4,
-	"i": 1,
-	"j": 8,
-	"k": 5,
-	"l": 1,
-	"m": 3,
-	"n": 1,
-	"o": 1,
-	"p": 3,
-	"q": 10,
-	"r": 1,
-	"s": 1,
-	"t": 1,
-	"u": 1,
-	"v": 4,
-	"w": 4,
-	"x": 8,
-	"y": 4,
-	"z": 10,
+var scores = map[rune]int{
+	'a': 1,
+	'b': 3,
+	'c': 3,
+	'd': 2,
+	'e': 1,
+	'f': 4,
+	'g': 2,
+	'h': 4,
+	'i': 1,
+	'j': 8,
+	'k': 5,
+	'l': 1,
+	'm': 3,
+	'n': 1,
+	'o': 1,
+	'p': 3,
+	'q': 10,
+	'r': 1,
+	's': 1,
+	't': 1,
+	'u': 1,
+	'v': 4,
+	'w': 4,
+	'x': 8,
+	'y': 4,
+	'z': 10,
 }
 
 func Score(word string) (score int) {
 	for _, c := range word {
-		score += scores[strings.ToLower(string(c))]
+		score += scores[unicode.ToLower(c)]
 	}
 	return
 }
