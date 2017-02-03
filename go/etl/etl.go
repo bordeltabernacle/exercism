9b0072f9b51dc9cde27d5eca95e3e52d6d76d219
Rob Phoenix <robphoenix@protonmail.com>
2017-02-03 16:19:01 +0000

    clearer var names

--- go/etl/etl.go
+++ go/etl/etl.go
@@ -4,9 +4,9 @@ import "strings"
 
 func Transform(input map[int][]string) map[string]int {
 	output := make(map[string]int)
-	for k, v := range input {
-		for _, el := range v {
-			output[strings.ToLower(el)] = k
+	for score, letters := range input {
+		for _, letter := range letters {
+			output[strings.ToLower(letter)] = score
 		}
 	}
 	return output
