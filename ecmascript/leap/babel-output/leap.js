585735ac1562888264cef772f6717fce13d81ffd
Rob Phoenix <robphoenix@protonmail.com>
2017-02-06 16:36:20 +0000

    leap in ecmascript

---
+++ ecmascript/leap/babel-output/leap.js
@@ -0,0 +1,11 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+function isLeapYear(year) {
+    return year % 400 == 0 || year % 4 == 0 && year % 100 != 0;
+};
+
+exports["default"] = isLeapYear;
+module.exports = exports["default"];
