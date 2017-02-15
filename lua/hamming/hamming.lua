198c49e6de58878b6d3b7f45cd0c2e90c0ccd080
Rob Phoenix <rob@robphoenix.com>
2017-02-15 12:10:30 +0000

    further changes for knowledge

--- lua/hamming/hamming.lua
+++ lua/hamming/hamming.lua
@@ -1,14 +1,12 @@
-local Hamming = {}
-
-function Hamming.compute(a, b)
-	local d = 0
-	if #a ~= #b then return -1 end
-	for i = 1, #a do
-		if a:byte(i) ~= b:byte(i) then
-			d = d + 1
-		end
-	end
-	return d
-end
-
-return Hamming
+return {
+  compute = function(a, b)
+    local d = 0
+    if #a ~= #b then return -1 end
+    for i = 1, #a do
+      if a:byte(i) ~= b:byte(i) then
+        d = d + 1
+      end
+    end
+    return d
+  end
+}
