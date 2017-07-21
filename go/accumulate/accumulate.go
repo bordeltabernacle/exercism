555e1635614efb8626352eb39226684c113a897a
Rob Phoenix <rob@robphoenix.com>
2017-07-21 15:19:45 +0100

    Add error handling exercise

--- go/accumulate/accumulate.go
+++ go/accumulate/accumulate.go
@@ -3,8 +3,9 @@ package accumulate
 const testVersion = 1
 
 func Accumulate(input []string, f func(string) string) []string {
+	output := make([]string, len(input))
 	for k, v := range input {
-		input[k] = f(v)
+		output[k] = f(v)
 	}
-	return input
+	return output
 }
