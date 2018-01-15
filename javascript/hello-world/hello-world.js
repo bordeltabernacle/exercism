734d84d3f7f5d73f1448555f3ee2bb36c147034c
Rob Phoenix <rob.phoenix@bet365.com>
2018-01-15 20:56:50 +0000

    javascript: submit two-fer

--- javascript/hello-world/hello-world.js
+++ javascript/hello-world/hello-world.js
@@ -1,10 +1,10 @@
-'use strict';
+"use strict";
 
 var HelloWorld = function() {};
 
 HelloWorld.prototype.hello = function(name) {
-  name = name || 'World';
-  return 'Hello, ' + name + '!';
+  name = name || "World";
+  return "Hello, " + name + "!";
 };
 
 module.exports = HelloWorld;
