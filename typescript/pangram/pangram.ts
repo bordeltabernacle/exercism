54e8e96e55ac626c84a6ecf52a80d2b3ebb79c59
Rob Phoenix <rob@robphoenix.com>
2018-02-08 07:07:36 +0000

    TypeScript: pangram: use a map

--- typescript/pangram/pangram.ts
+++ typescript/pangram/pangram.ts
@@ -1,17 +1,21 @@
 class Pangram {
-    private sentence: string
+    private sentence: string[]
     private alphabet: string[] = Array.from("abcdefghijklmnopqrstuvwxyz")
 
     constructor(sentence: string) {
-        this.sentence = sentence
+        this.sentence = Array.from(sentence)
     }
 
     isPangram(): boolean {
         if (this.sentence.length < 26) {
             return false
         }
-        return this.alphabet.every((letter: string) =>
-            this.sentence.toLowerCase().includes(letter)
+        const sentenceMap = new Map<string, boolean>()
+        this.sentence.forEach((key: string) => {
+            sentenceMap.set(key.toLowerCase(), true)
+        })
+        return this.alphabet.every(
+            (letter: string) => sentenceMap.get(letter) !== undefined
         )
     }
 }
