5b5e3f81883ce330832d38f8c816459bc2e3daa7
Rob Phoenix <rob@robphoenix.com>
2018-02-09 10:17:41 +0000

    JavaScript: pangram: use class

--- javascript/pangram/pangram.js
+++ javascript/pangram/pangram.js
@@ -1,11 +1,13 @@
-let Pangram = function(text) {
-  this.text = text
-
-  this.isPangram = () => {
-    return (
-      new Set(Array.from(this.text.toLowerCase().replace(/[^a-z]/g, "")))
-        .size === 26
+class Pangram {
+  constructor(text) {
+    this.text = text
+    this._set = new Set(
+      Array.from(this.text.toLowerCase().replace(/[^a-z]/g, ""))
     )
+  }
+
+  isPangram() {
+    return this._set.size === 26
   }
 }
 
