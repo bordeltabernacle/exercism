57c1afeeea7b519ea05e11d49cbdea1f048cf105
Rob Phoenix <rob.phoenix@bet365.com>
2018-08-13 10:14:19 +0100

    TypeScript: hello-world v2

--- typescript/hello-world/hello-world.ts
+++ typescript/hello-world/hello-world.ts
@@ -1,6 +1,6 @@
 class HelloWorld {
-    static hello(name?: string) {
-        return `Hello, ${name || "World"}!`
+    static hello(name: string = `World`) {
+        return `Hello, ${name}!`
     }
 }
 
