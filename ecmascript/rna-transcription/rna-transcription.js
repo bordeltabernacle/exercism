f27488b286412132bb6b2c1514e4ce8fb3051333
Rob Phoenix <rob.phoenix@bet365.com>
2018-08-13 09:34:12 +0100

    restart TypeScript track v2.0

--- ecmascript/rna-transcription/rna-transcription.js
+++ ecmascript/rna-transcription/rna-transcription.js
@@ -1,13 +1,24 @@
 class Transcriptor {
-  static toRna(dna) {
-    return [...dna]
-      .map((x) => {
-        const nucleotide = { G: 'C', C: 'G', T: 'A', A: 'U' }[x];
-        if (nucleotide === undefined) {
-          throw Error('Invalid input DNA.');
-        }
-        return nucleotide;
-      })
+  constructor() {
+    this.translations = {
+      G: 'C',
+      C: 'G',
+      T: 'A',
+      A: 'U',
+    };
+  }
+
+  transcribe(nucleotide) {
+    const complement = this.translations[nucleotide];
+    if (complement === undefined) {
+      throw Error('Invalid input DNA.');
+    }
+    return complement;
+  }
+
+  toRna(dna) {
+    return Array.from(dna)
+      .map(x => this.transcribe(x))
       .join('');
   }
 }
