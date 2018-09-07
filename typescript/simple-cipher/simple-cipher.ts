12f2e3e180accb2080d311863d48e43c91940c5c
Rob Phoenix <rob@robphoenix.com>
2018-09-07 20:48:18 +0100

    TypeScript: Complete simple-cipher

--- typescript/simple-cipher/simple-cipher.ts
+++ typescript/simple-cipher/simple-cipher.ts
@@ -1,25 +1,43 @@
 class SimpleCipher {
   private alphabet: string = `abcdefghijklmnopqrstuvwxyz`
-  key: string
+  private key: string
 
   constructor(userKey?: string) {
     this.key = userKey || this.randomKey()
+    if (userKey === "" || !this.key.match(/^[a-z]+$/)) {
+      throw Error("Bad key")
+    }
   }
 
-  private randomKey() {
+  private randomKey(): string {
     return Array.from(Array(100)).reduce(
-      (key) => key + this.alphabet[Math.floor(Math.random() * 26)],
+      (key: string) => `${key}${this.alphabet[Math.floor(Math.random() * 26)]}`,
       ""
     )
   }
 
+  private convertChar(char: string, index: number, sign: number): string {
+    let offset: number =
+      this.alphabet.indexOf(char) +
+      sign * this.alphabet.indexOf(this.key[index % this.key.length])
+
+    offset = offset >= 26 ? (offset -= 26) : offset
+    offset = offset < 0 ? (offset += 26) : offset
+
+    return this.alphabet[offset]
+  }
+
+  private convert(text: string, sign: number) {
+    return Array.from(text).reduce((result, char, i) => {
+      return `${result}${this.convertChar(char, i, sign)}`
+    }, "")
+  }
+
   encode(plaintext: string): string {
-    return plaintext
+    return this.convert(plaintext, 1)
   }
 
   decode(ciphertext: string): string {
-    return ciphertext
+    return this.convert(ciphertext, -1)
   }
 }
-
-export default SimpleCipher
